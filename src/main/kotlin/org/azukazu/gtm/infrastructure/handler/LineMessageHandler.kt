5f0bfa306a35c51d4921375b6010007fd04d73fe
azu-kazu <azu.kazu.max@gmail.com>
2024-12-27 13:16:17 +0900

    Handler から呼ぶアプリケーションサービスを減らす

--- src/main/kotlin/org/azukazu/gtm/infrastructure/handler/LineMessageHandler.kt
+++ src/main/kotlin/org/azukazu/gtm/infrastructure/handler/LineMessageHandler.kt
@@ -5,16 +5,13 @@ import com.linecorp.bot.model.event.message.TextMessageContent
 import com.linecorp.bot.spring.boot.annotation.EventMapping
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
 import lombok.extern.slf4j.Slf4j
-import org.azukazu.gtm.application.JudgeNeedsProcessApplicationService
 import org.azukazu.gtm.application.NotifyLineOfErrorMessageApplicationService
 import org.azukazu.gtm.application.NotifyLineOfImageSearchResultApplicationService
-import org.azukazu.gtm.domain.model.search_word.SearchWord
 import org.azukazu.gtm.application.ReplyToken
 
 @Slf4j
 @LineMessageHandler
 class LineMessageHandler(
-    private val judgeNeedsProcessApplicationService: JudgeNeedsProcessApplicationService,
     private val notifyLineOfImageSearchResultApplicationService: NotifyLineOfImageSearchResultApplicationService,
     private val notifyLineOfErrorMessageApplicationService: NotifyLineOfErrorMessageApplicationService
 ) {
@@ -25,16 +22,11 @@ class LineMessageHandler(
     @EventMapping
     fun handleTextMessageEvent(request: MessageEvent<TextMessageContent>) {
 
-        val text = request.message.text
-
-        if (!judgeNeedsProcessApplicationService.judge(text)) {
-            return
-        }
-
         runCatching {
             notifyLineOfImageSearchResultApplicationService.notifyImageAtRandom(
                 ReplyToken(request.replyToken),
-                SearchWord.of(text))
+                request.message.text
+            )
         }.onFailure { throwable ->
             notifyLineOfErrorMessageApplicationService.notify(
                 ReplyToken(request.replyToken),
